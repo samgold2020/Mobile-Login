42688fb515f545495a5c766cfe80da4ed534df93
Sam Goldstein <sgoldstein312@gmail.com>
2022-05-22 23:28:21 -0500

    remove password validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,3 @@
 export const emailValidation = new RegExp(
   /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i,
 );
-
-export const passValidation = new RegExp(
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[\/@$!%*#?&])[A-Za-z\d\/@$!%*#?&]{8,}$/,
-);
